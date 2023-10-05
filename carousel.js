d0fb05785390ce19387401067f0cc15747765a8f
Miguel Arandia <143758416+marandia513@users.noreply.github.com>
2023-10-04 21:03:19 -0300

    agregado el carusel
    agregado el carusel

---
+++ carousel.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+const carousel = document.querySelector('.carousel-container');
+const slides = document.querySelectorAll('.carousel-slide');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(${100 * (i - index)}%)`;
+  });
+}
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  showSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+});
+
+showSlide(currentIndex);
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 5000); // Cambia de diapositiva cada 5 segundos (5000 milisegundos)
+}
+
+
