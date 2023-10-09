a7851409a44441fe0807d1a09cdffa3e77d99378
Miguel Arandia <143758416+marandia513@users.noreply.github.com>
2023-10-09 13:15:26 -0300

    modificaciones en js
    modificaciones en js

--- HVII Version 10.0.1/showdata.js
+++ HVII Version 10.0.1/showdata.js
@@ -1,26 +1,18 @@
-window.onload = function () {
-    let showdata = document.querySelector('.table');
-    var args = location.search.substr(1).split('&');
-
-    for (var i = 0; i < args.length; ++i) {
-        var parts = args[i].split('=');
-        if (parts.length === 2) { // Verifica que haya dos partes (nombre y valor)
-            var field = decodeURIComponent(parts[0]); // Decodifica el nombre
-            var value = decodeURIComponent(parts[1]); // Decodifica el valor
-
-            // Crea una nueva fila de tabla para cada par de parámetros
-            var row = document.createElement('tr');
-            var nameCell = document.createElement('td');
-            nameCell.textContent = field;
-            var valueCell = document.createElement('td');
-            valueCell.textContent = value;
-
-            // Agrega las celdas a la fila
-            row.appendChild(nameCell);
-            row.appendChild(valueCell);
-
-            // Agrega la fila a la tabla
-            showdata.appendChild(row);
+//*window.onload = function () {
+    document.addEventListener('DOMContentLoaded', function() {
+        var params = new URLSearchParams(location.search);
+        var tableBody = document.getElementById('dataBody');
+    
+        for (var pair of params.entries()) {
+            var field = pair[0];
+            var value = pair[1] || "null";
+    
+            var newRow = tableBody.insertRow();
+            var cell1 = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+            cell1.textContent = field;
+            cell2.textContent = value;
         }
-    }
-}
+    });
+    
+    
