4c4fe7cf3e528985b68ebb7ba3b048cd59404de1
Miguel Arandia <143758416+marandia513@users.noreply.github.com>
2023-10-22 17:07:53 -0300

    Add files via upload

---
+++ carousel.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+const carousel = document.querySelector('.carousel-container');
+const slides = document.querySelectorAll('.carousel-slide');
+
+
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(${100 * (i - index)}%)`;
+  });
+}
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  showSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+});
+
+showSlide(currentIndex);
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 5000); // Cambia de diapositiva cada 5 segundos (5000 milisegundos)
+
+const menuToggle = document.getElementById('menu-toggle');
+const navList = document.getElementById('nav-list');
+  menuToggle.addEventListener('click', () => {
+      navList.classList.toggle('active');
+  });
+}
